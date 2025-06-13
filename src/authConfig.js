cd4b5a43fa23a38d554628314f0fe2b493c59631
93372377 <142664513+93372377@users.noreply.github.com>
2025-06-13 21:37:39 +0200

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,11 +1,16 @@
 export const msalConfig = {
   auth: {
-    clientId: "4d661288-c66e-45a8-a8a3-b76b795be7f9",
-    authority: "https://login.microsoftonline.com/a00de4ec-48a8-43a6-be74-e31274e2060d",
-    redirectUri: "https://pwcautomation.netlify.app"
+    clientId: 'YOUR_CLIENT_ID',
+    authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID', // YOUR tenant ID
+    redirectUri: window.location.origin
+  },
+  cache: {
+    cacheLocation: 'localStorage',
+    storeAuthStateInCookie: true
   }
 };
 
 export const loginRequest = {
-  scopes: ["Files.ReadWrite.All", "Sites.ReadWrite.All", "User.Read"]
+  scopes: ['User.Read'],
+  prompt: 'login' // always ask for login
 };
